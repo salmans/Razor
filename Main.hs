ad870fbca17ff6d46e8d4765bf1b07d1fb195d1b
Taymon A. Beal <taymonbeal@wpi.edu>
2014-02-07 23:31:00 -0500

    Added an expression system for models.

--- Main.hs
+++ Main.hs
@@ -5,6 +5,7 @@ import System.Time
 import System.IO
 import Debug.Trace
 import Control.Exception 
+import Control.Monad
 import Data.List
 import qualified Data.Either as Either
 import qualified Data.Maybe as Maybe
@@ -33,30 +34,52 @@ main = do
       formulae <- readFormulae inputPath
       putStrLn "Theory:"
       putStrLn $ show formulae
-      modelLoop (runChase Nothing formulae) Map.empty
+      case (runChase Nothing formulae) of
+        [] -> putStrLn "No models found."
+        row@(model:_) -> do
+          putStrLn "First model:"
+          putStrLn $ show model
+          let origin = GraphLoc row 0
+          modelLoop Map.empty origin origin
     otherwise -> error "Usage: ./Main filename"
 
 readFormulae :: FilePath -> IO [Sequent]
 readFormulae inputFileName = readFile inputFileName >>= return . map parseSequent . filter Utils.Utils.isRealLine . lines
 
-data UserCommand = Add Obs | Next | Load String | Save String deriving Read
+data UserCommand = Show ModelExpr | Store String ModelExpr | Exit deriving Read
+data ModelExpr = Origin | Last | ModelVar String | Add ModelExpr Obs | Next ModelExpr deriving Read
+data GraphLoc = GraphLoc [Problem] Int
 
-modelLoop :: [Problem] -> Map.Map String [Problem] -> IO ()
-modelLoop probs varBindings = case probs of
-  [] -> putStrLn "No more models found."
-  (prob@Problem {problemModel = oldModel, problemLastConstant = oldConst}):rest -> do
-    putStrLn "Model:"
-    putStrLn $ show oldModel
-    putStr "> "
-    hFlush stdout
-    userInput <- getLine
-    if Utils.Utils.isNonEmptyLine userInput then
-      case (read userInput) of
-        Add constraint -> let (newModel, _, newConst) = add oldModel oldConst [constraint] $ Just UserProv in
-          modelLoop (runChaseWithProblem prob {problemModel = newModel, problemLastConstant = newConst}) varBindings
-        Next -> modelLoop rest varBindings
-        Load var -> case (Map.lookup var varBindings) of
-          Just oldProbs -> modelLoop oldProbs varBindings
-          Nothing -> putStrLn "Variable not found." >> modelLoop probs varBindings
-        Save var -> modelLoop probs $ Map.insert var probs varBindings
-      else return ()
+resolve :: ModelExpr -> Map.Map String GraphLoc -> GraphLoc -> GraphLoc -> Maybe GraphLoc
+resolve expr bindings origin last = case expr of
+  Origin -> Just origin
+  Last -> Just last
+  ModelVar name -> Map.lookup name bindings
+  Add subExpr obs -> do
+    GraphLoc row index <- resolve subExpr bindings origin last
+    let prob@Problem {problemModel = oldModel, problemLastConstant = oldConst} = row !! index
+        (newModel, _, newConst) = add oldModel oldConst [obs] $ Just UserProv
+    case (runChaseWithProblem prob {problemModel = newModel, problemLastConstant = newConst}) of
+      [] -> Nothing
+      newRow -> Just $ GraphLoc newRow 0
+  Next subExpr -> do
+    GraphLoc row index <- resolve subExpr bindings origin last
+    let nextIndex = succ index
+    if nextIndex < length row then Just $ GraphLoc row nextIndex else Nothing
+
+modelLoop :: Map.Map String GraphLoc -> GraphLoc -> GraphLoc -> IO ()
+modelLoop bindings origin last = do
+  let continue = modelLoop bindings origin last
+  putStr "> "
+  hFlush stdout
+  userInput <- getLine
+  if Utils.Utils.isNonEmptyLine userInput then
+    case (read userInput) of
+      Show expr -> case (resolve expr bindings origin last) of
+        Just loc@(GraphLoc row index) -> putStrLn (show (problemModel (row !! index))) >> modelLoop bindings origin loc
+        Nothing -> putStrLn "Model not found." >> continue
+      Store name expr -> case (resolve expr bindings origin last) of
+        Just loc -> modelLoop (Map.insert name loc bindings) origin last
+        Nothing -> putStrLn "Model not found." >> continue
+      Exit -> return ()
+    else continue
