d718b01928e4e42e0020390f084c37494f468c7f
ryandanas <ryandanas@gmail.com>
2014-06-17 13:26:27 -0400

    Table references now included in the XML model. Just need to include the actual table data.

--- Chase/Problem/XMLModel.hs
+++ Chase/Problem/XMLModel.hs
@@ -4,12 +4,14 @@
 module Chase.Problem.XMLModel where
 
 -- General Modules
-
+import Data.Map (fromList, toList)
+import Data.Maybe
 -- Logic Modules
-
+import Formula.SyntaxGeo
+import Utils.GeoUtilities
 -- Chase Modules
 import Chase.Problem.Model
-import Chase.Problem.RelAlg.RelAlg
+import Chase.Problem.RelAlg.IRelAlg
 import Chase.Problem.BaseTypes
 -- HXT Library Modules
 import Text.XML.HXT.Arrow.Pickle
@@ -36,14 +38,53 @@ xpModel =
             xpProvInfo
             xpElemHistory
 
---instance XmlPickler Tables where xpickle = xpTables
+{-
+type Tables = Map TableRef Table
+-}
 xpTables :: PU Tables
-xpTables = xpElem "tables" (xpZero "Tables not implemented")
+xpTables = 
+  xpWrap ( fromList, toList ) $
+  xpList $
+  xpElem "table" $
+  xpPair  xpTableRef
+          xpTable
 
---instance XmlPickler ProvInfo where xpickle = xpProvInfo
+-- Maps a table ref type to a string
+tableType :: TableRef -> String
+tableType (ConTable s)  = "Constant"
+tableType (RelTable s)  = "Relation"
+tableType (FunTable s)  = "Function"
+tableType (DomTable)    = "Domain"
+tableType (TmpTable)    = "Temporary"
+-- Maps a string to a table ref type
+typeTable :: String -> Maybe Sym -> TableRef
+typeTable "Constant" (Just s)  = (ConTable s)
+typeTable "Relation" (Just s)  = (RelTable s)
+typeTable "Function" (Just s)  = (FunTable s)
+typeTable "Domain"    Nothing  = (DomTable)
+typeTable "Temporary" Nothing  = (TmpTable)
+{-
+data TableRef  = ConTable Sym -- constant tables
+               | RelTable Sym -- relation tables
+               | FunTable Sym -- function tables
+               | DomTable -- A table containing elements of the domain
+               | TmpTable -- A temporary table denoting equality between 
+                          -- elements (C-rules in Bachmair et al. definitions)
+-}
+xpTableRef :: PU TableRef
+xpTableRef =
+  xpWrap  ( \ ((reftype, refname)) -> (typeTable reftype refname)
+    , \ t -> (tableType t, 
+              tableName t)
+    ) $
+  xpPair  (xpAttr "type" xpText)
+          (xpOption (xpAttr "name" xpText))
+
+xpTable :: PU Table
+xpTable = xpElem "table" (xpZero "Not implemented")
+
 xpProvInfo :: PU ProvInfo
-xpProvInfo = xpElem "provinfo" (xpZero "ProvInfo not implemented")
+xpProvInfo = xpElem "provinfo" (xpZero "Not implemented")
 
---instance XmlPickler ElemHistory where xpickle = xpElemHistory
 xpElemHistory :: PU ElemHistory
-xpElemHistory = xpElem "elemhist" (xpZero "ElemHistory not implemented")
+xpElemHistory = xpElem "elemhist" (xpZero "Not implemented")
