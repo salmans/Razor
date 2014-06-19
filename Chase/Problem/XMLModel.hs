069b7a4169fd1ee2f1339b4bd7bc902b7855f26e
ryandanas <ryandanas@gmail.com>
2014-06-19 14:36:50 -0400

    The current model structure is now completely converted into XML. However, some trimming / formating changes may be made to better structure how the data is represented.

--- Chase/Problem/XMLModel.hs
+++ Chase/Problem/XMLModel.hs
@@ -1,11 +1,12 @@
 {-
   This module defines an instance of a Chase Model serialized into/outof an XML String
   NOTE: Going from XML to Haskell Data Structures has not been tested
+  TODO: Type Attributes should somehow be removed (possibly by becoming the element name)
 -}
 module Chase.Problem.XMLModel where
 
 -- General Modules
-import Data.Map (fromList, toList)
+import Data.Map (fromList, toList, Map)
 import Data.Maybe
 -- Logic Modules
 import Formula.SyntaxGeo
@@ -38,7 +39,7 @@ xpModel =
     ) $
   xpTriple  xpTables
             xpProvInfo
-            xpElemHistory
+            xpElemHistories
 
 {-
 type Tables = Map TableRef Table
@@ -62,27 +63,26 @@ type Sym = String
 -}
 xpTableRef :: PU TableRef
 xpTableRef =
-  xpWrap  ( \ ((reftype, refname)) -> (typeTable reftype refname)
-    , \ t -> (tableType t, 
-              tableName t)
+  xpWrap  ( \ ((reftype, refname)) -> (implodeTableRef (reftype, refname))
+    , \ t -> (explodeTableRef t)
     ) $
   xpPair  (xpAttr "type" xpText)
           (xpOption (xpAttr "name" xpText))
 -- TODO: I think there's a way to avoid using the two following functions for disjunct types
--- Maps a table ref type to a string
-tableType :: TableRef -> String
-tableType (ConTable s)  = "Constant"
-tableType (RelTable s)  = "Relation"
-tableType (FunTable s)  = "Function"
-tableType (DomTable)    = "Domain"
-tableType (TmpTable)    = "Temporary"
--- Maps a string to a table ref type
-typeTable :: String -> Maybe Sym -> TableRef
-typeTable "Constant" (Just s)  = (ConTable s)
-typeTable "Relation" (Just s)  = (RelTable s)
-typeTable "Function" (Just s)  = (FunTable s)
-typeTable "Domain"    Nothing  = (DomTable)
-typeTable "Temporary" Nothing  = (TmpTable)
+-- Maps a TableRef to an XML friendly tuple
+explodeTableRef :: TableRef -> (String, Maybe Sym)
+explodeTableRef (ConTable s)  = ("Constant", Just s)
+explodeTableRef (RelTable s)  = ("Relation", Just s)
+explodeTableRef (FunTable s)  = ("Function", Just s)
+explodeTableRef (DomTable)    = ("Domain", Nothing)
+explodeTableRef (TmpTable)    = ("Temporary", Nothing)
+-- Maps an XML tuple to a TableRef
+implodeTableRef :: (String , Maybe Sym) -> TableRef
+implodeTableRef ("Constant", (Just s))  = (ConTable s)
+implodeTableRef ("Relation", (Just s))  = (RelTable s)
+implodeTableRef ("Function", (Just s))  = (FunTable s)
+implodeTableRef ("Domain", Nothing)     = (DomTable)
+implodeTableRef ("Temporary", Nothing)  = (TmpTable)
 
 {-
 type Table = DB.Set Record
@@ -116,17 +116,125 @@ xpElt =
       ) $
   (xpAttr "elt" xpText)
 
+{-
+data ProvInfo = ProvInfo { provInfoData     :: Map.Map Obs [Prov]
+                         , provInfoLastTag  :: ProvTag }
+type ProvTag = Int
+-}
 xpProvInfo :: PU ProvInfo
-xpProvInfo = xpElem "provinfo" (xpZero "Not implemented")
+xpProvInfo =
+  xpElem "provenanceinfos" $
+  xpWrap ( \ ((pidata, pilasttag)) -> ProvInfo pidata pilasttag
+    , \ t -> (provInfoData t,
+              provInfoLastTag t)
+    ) $
+  xpPair  xpProvInfoData
+          (xpAttr "total" xpPrim)
+
+{-
+provInfoData     :: Map.Map Obs [Prov]
+-}
+xpProvInfoData :: PU (Map Obs [Prov])
+xpProvInfoData = 
+  xpWrap ( fromList, toList ) $
+  xpList $
+  xpElem "provinfo" $
+  xpPair  xpObs
+          (xpList xpProv)
+
+{-
+data Obs = Eql Term Term  -- Two terms are equal
+         | Fct Atom       -- Atm is true as a fact
+-}
+xpObs :: PU Obs
+xpObs = 
+  xpElem "obs" $
+  xpWrap ( \ ((obstype, t1, t2, atom)) -> implodeObs (obstype, t1, t2, atom)
+      , \ t -> (explodeObs t)
+  ) $
+  xp4Tuple  (xpAttr "type" xpText)
+            (xpOption xpSkolemTerm)
+            (xpOption xpSkolemTerm)
+            (xpOption xpAtom)
+-- TODO: again, better way to do this?
+-- Maps an observation to a xml-friendly value tuple
+explodeObs :: Obs -> (String, Maybe Term, Maybe Term, Maybe Atom)
+explodeObs (Eql t1 t2)         = ("Equality", Just t1, Just t2, Nothing)
+explodeObs (Fct a)             = ("Fact", Nothing, Nothing, Just a)
+-- Unmaps a observation to a xml-friendly value tuple
+implodeObs :: (String, Maybe Term, Maybe Term, Maybe Atom) -> Obs
+implodeObs ("Equality", Just t1, Just t2, Nothing)  = (Eql t1 t2)
+implodeObs ("Fact", Nothing, Nothing, Just a)       = (Fct a)
+
+{-
+data Atom = R Sym [Term]
+          | F Sym [Term]
+-}
+xpAtom :: PU Atom
+xpAtom =
+  xpElem "atom" $
+  xpWrap ( \ ((atomtype, name, terms)) -> implodeAtom (atomtype, name, terms)
+      , \ t -> (explodeAtom t)
+  ) $
+  xpTriple  (xpAttr "type" xpText)
+            (xpAttr "name" xpText)
+            (xpList xpSkolemTerm)
+-- TODO: again, better way to do this?
+-- Maps an atom to a xml-friendly value tuple
+explodeAtom :: Atom -> (String, String, [Term])
+explodeAtom (R s terms)              = ("Relation", s, terms)
+explodeAtom (F s terms)              = ("Function", s, terms)
+-- Unmaps a atom from a xml-friendly value tuple
+implodeAtom :: (String, String, [Term]) -> Atom
+implodeAtom ("Relation", s, terms) = (R s terms) 
+implodeAtom ("Function", s, terms) = (F s terms) 
+
+{-
+data Prov = ChaseProv ProvTag ID Sub
+          | UserProv
+type ProvTag = Int
+type ID = Int
+-}
+xpProv :: PU Prov
+xpProv =
+  xpElem "prov" $
+  xpWrap ( \ ((provtype, tag, i, sub)) -> implodeProv (provtype, tag, i, sub)
+      , \ t -> (explodeProv t)
+  ) $
+  xp4Tuple  (xpAttr "type" xpText)
+            (xpOption (xpAttr "tag" xpPrim))
+            (xpOption (xpAttr "id" xpPrim))
+            (xpOption xpSub)
+-- TODO: again, better way to do this?
+-- Maps a provenance to a xml-friendly value tuple
+explodeProv :: Prov -> (String, Maybe Int, Maybe Int, Maybe Sub)
+explodeProv (ChaseProv tag i sub)             = ("Chase", Just tag, Just i, Just sub)
+explodeProv (UserProv)              = ("User", Nothing, Nothing, Nothing)
+-- Unmaps a provenance from a xml-friendly value tuple
+implodeProv :: (String, Maybe Int, Maybe Int, Maybe Sub) -> Prov
+implodeProv ("Chase", Just tag, Just i, Just sub) = (ChaseProv tag i sub) 
+implodeProv ("User", Nothing, Nothing, Nothing) = (UserProv)
+
+{-
+type Sub = Map Var Term
+type Var = String
+-}
+xpSub :: PU Sub
+xpSub =
+  xpWrap ( fromList, toList ) $
+  xpList $
+  xpElem "sub" $
+  xpPair  (xpAttr "var" xpText)
+          (xpSkolemTerm)
 
 {-
 type ElemHistory = [(Elem, SkolemTerm)]
 -}
-xpElemHistory :: PU ElemHistory
-xpElemHistory = (xpList xpElemProvInfo)
+xpElemHistories :: PU ElemHistory
+xpElemHistories = (xpList xpElemHistory)
 
-xpElemProvInfo :: PU (Elem, SkolemTerm)
-xpElemProvInfo =
+xpElemHistory :: PU (Elem, SkolemTerm)
+xpElemHistory =
   xpElem "history" $
   xpWrap ( \ ((e, st)) -> (e, st)
     , \ t -> (fst t, snd t)
@@ -143,7 +251,7 @@ data Term = Var Var
 -}
 xpSkolemTerm :: PU SkolemTerm
 xpSkolemTerm = 
-  xpElem "SkolemTerm" $
+  xpElem "term" $
   xpWrap ( \ ((termtype, value, moreterms)) -> implodeSkolemTerm (termtype, value, moreterms)
     , \ t -> (explodeSkolemTerm t)
     ) $
