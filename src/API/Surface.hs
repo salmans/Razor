53b7e54678f0ce1ca05d5180e1434f0093befd88
ryandanas <ryandanas@wpi.edu>
2014-10-22 12:36:00 -0400

    origin(s)(*) moved over to new surface syntax. the repl printout isnt as pretty, but the return values are refactored for easy passing to the CLI / GUI.

--- src/API/Surface.hs
+++ src/API/Surface.hs
@@ -15,24 +15,69 @@ import Data.Either
 import Data.Maybe
 import System.Environment
 
-type UState = (Theory, ProvInfo, SATIteratorType)
+data UError = UErr String
+type UState = (Theory, ProvInfo, SATIteratorType, Model)
+type UTheorySubs = [Maybe Sequent]
 
 getConfig :: IO Config
 getConfig = do 
 	args <- getArgs
 	parseConfig args
-getStartState :: Config -> IO (Either String UState)
+getStartState :: Config -> IO (Either UError UState)
 getStartState config = do
   case (configInput config) of
-    Nothing -> return $ Left "No input file specified"
+    Nothing -> return $ Left (UErr "No input file specified")
     Just infile -> do
-    	input <- readFile infile
-    	thy <- parseTheory config input
-    	case thy of
-    		Just theory -> do
-    			let (base, prov, prop) = generateGS config theory
-     			let stream = modelStream prop 
-     			return $ Right (theory, prov, stream)
-     		Nothing -> return $ Left "Unable to parse input theory!"
+      input <- readFile infile
+      theory <- parseTheory config input
+      case theory of
+        Just thy -> do
+          let (base, prov, prop) = generateGS config thy
+          let stream = modelStream prop
+          case (nextModel stream) of
+            (Nothing, stream') -> return $ Left (UErr "no minimal models available")
+            (Just model', stream') -> return $ Right (thy, prov, stream', model')
+        Nothing -> return $ Left (UErr "Unable to parse input theory!")
 
---getNextModel :: UState -> Either String (Theory, ProvInfo, SATIteratorType)
+getNextModel :: UState -> Either UError UState
+getNextModel state@(thy, prov, stream, model) = case (nextModel stream) of
+  (Nothing, stream') -> Left (UErr "no more minimal models")
+  (Just model', stream') -> Right (thy, prov, stream', model')
+
+getOrigin :: UState -> (Bool, Bool, Term) -> [(Term, Either UError UTheorySubs)]
+getOrigin state@(theory, prov, stream, model) (isall, isrec, term) = origin (theory, prov, model) (isall, isrec) [term] 
+origin :: (Theory, ProvInfo, Model) -> (Bool, Bool) -> [Term] -> [(Term, Either UError UTheorySubs)]
+origin _ _ [] = []
+origin substate (isall,isrec) terms = do
+  term <- terms
+  case name substate isall term of
+    Left err -> return (term, Left err)
+    Right (namedtheory, nextterms) -> do
+      let termorigin = (term, Right namedtheory)
+      case isrec of
+        False -> return termorigin
+        True -> concat $ return $ termorigin:(origin substate (isall,isrec) (concat (return nextterms)))
+name :: (Theory, ProvInfo, Model) -> Bool -> Term -> Either UError (UTheorySubs, [Term])
+name (theory, prov, model) isall term = do
+  case (getEqualElements model term) of
+    [] -> Left (UErr ("element "++(show term)++" not in the current model"))
+    eqelms -> do
+      case (getSkolemTrees prov model term) of
+        [] -> Left (UErr ("no provenance information for element "++(show term)++"\n"))
+        skolemtrees -> do
+          -- Look at the first or all skolem trees depending on user input
+          case isall of
+            True -> do
+              let (actualelm,_,_) = head skolemtrees
+              let allelms = map (\(e, h, r)->e) skolemtrees
+              let skolemnext = concat (map (\(e, h, r)->r) skolemtrees)
+              let names = (map (\(e, h, r)->(actualelm, h, r)) skolemtrees) 
+              let namedtheory = nameTheory theory names
+              let nextterms = (map (\e->(Elem e)) skolemnext)
+              return (namedtheory, nextterms)
+            False -> do
+              let (elm, skolemhead, skolemnext) = (head skolemtrees)
+              let namedtheory = (nameTheory theory [(elm, skolemhead, skolemnext)])
+              let nextterms = (map (\e->(Elem e)) skolemnext)
+              return (namedtheory, nextterms)
+
--- src/REPL/Main.hs
+++ src/REPL/Main.hs
@@ -13,6 +13,7 @@ augmentation
 {-| PEDANDTIC / DISPLAY
 the text output for larger theories should still be managable (e^0, e^11 is a lot for each element)
 origin* should not print redundant info as it goes down the tree
+origin printouts are not as pretty due to refactoring of the REPL code for the CLI/GUI
 -}
 module Main where
 import API.Core
@@ -42,98 +43,56 @@ main = do
   putStrLn $ show config
   putStrLn "<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>"
   -- get the start state
-  mState <- getStartState config
-  case mState of
-    Left err -> error err
-    Right state@(theory, prov, stream) -> do
-      -- get the first model
-      case (nextModel stream) of
-        (Nothing, stream') -> (prettyPrint 0 ferror "no models found\n")
-        (Just model', stream') -> do
-          (prettyPrint 0 flow (show model'))
-          -- enter the repl
-          runInputT defaultSettings (loop (model', stream') prov theory)
+  startState <- getStartState config
+  case startState of
+    Left (UErr err) -> error err
+    Right state@(theory, prov, stream, model) -> do
+      (prettyPrint 0 flow (show model))
+      runInputT defaultSettings (loop state)
   -- exit display
   displayExit
 
-loop :: (Model, SATIteratorType) -> ProvInfo -> Theory -> InputT IO ()
-loop (model, stream) prov thy = do
-  let sameLoop = loop (model, stream) prov thy
-  let newLoop (model', stream') = (lift $ prettyPrint 0 flow (show model')) >> loop (model', stream') prov thy
+loop :: UState -> InputT IO ()
+loop state@(theory, prov, stream, model) = do
+  -- possible REPL loops
+  let sameLoop = loop state
+  let newLoop state'@(theory', prov', stream', model') = (lift $ prettyPrint 0 flow (show model')) >> loop state'
+  -- get input
   minput <- getInputLine "% "
+  -- parse input into a command and act depending on the case
   case minput of
       Nothing -> return ()
       Just command -> case (parseCommand command) of
+        -- display
         Display thing -> case thing of
-          DispTheory -> (lift $ mapM_ (\s-> prettyPrint 0 finput ((show s)++"\n")) thy) >> sameLoop
-          DispModel -> newLoop (model, stream)
+          DispTheory -> (lift $ mapM_ (\s-> prettyPrint 0 finput ((show s)++"\n")) theory) >> sameLoop
+          DispModel -> newLoop state
+        -- exploration
         Go explore -> case explore of
-          Next -> do
-            case (nextModel stream) of
-              (Nothing, stream') -> (lift $ prettyPrint 0 ferror "no more minimal models available\n") >> sameLoop
-              (Just model', stream') -> newLoop (model', stream')
+          Next -> case getNextModel state of
+            Left (UErr err) -> (lift $ prettyPrint 0 ferror (err++"\n")) >> sameLoop
+            Right state' -> newLoop state'
           Augment term -> (lift $ prettyPrint 0 ferror "not implemented\n") >> sameLoop
+        -- explanation
         Ask question -> case question of
-          Name isall isrec term -> (origin thy prov model [term] (isall,isrec,0)) >> sameLoop
-          Blame atom -> (justify thy prov model atom) >> sameLoop
+          Name isall isrec term -> do
+            let origins = getOrigin state (isall,isrec,term)
+            printOrigin theory origins (isall, isrec)
+            sameLoop
+          Blame atom -> (justify state atom) >> sameLoop
+        -- others
         Other utility -> case utility of
           Help -> (lift $ prettyPrint 0 foutput helpCommand) >> sameLoop
           Exit -> (lift $ prettyPrint 0 foutput "closing...\n") >> return ()
         SyntaxError err -> (lift $ prettyPrint 0 ferror (err++"\n")) >> sameLoop
 
+printOrigin :: Theory -> [(Term, Either UError UTheorySubs)] -> (Bool, Bool) -> InputT IO ()
+printOrigin thy origins (isall, isrec) = mapM_ (\(term, origin) -> case origin of
+  Left (UErr err) -> (lift $ prettyPrint 0 ferror (err++"\n"))
+  Right namedthy -> printDiff (thy,namedthy) ((show term),0,isall)) origins
 
--- Naming
-origin :: Theory -> ProvInfo -> Model -> [Term] -> (Bool, Bool, Int) -> InputT IO ()
-origin _ _ _ [] (_,_,_)= return ()
-origin thy prov mdl terms (isall,isrec,tabs) = do
-  nextterms <- (mapM (\t-> (name thy prov mdl t (isall,tabs))) terms)
-  if isrec
-    then origin thy prov mdl (concat nextterms) (isall,isrec,(tabs+1))
-    else return ()
-name :: Theory -> ProvInfo -> Model -> Term -> (Bool,Int) -> InputT IO ([Term])
-name thy prov mdl term (isall,tabs) = do
-  case (getEqualElements mdl term) of
-    [] -> (lift $ (prettyPrint tabs ferror ("element "++(show term)++" not in the current model\n"))) >> return []
-    eqelms -> do
-      case (getSkolemTrees prov mdl term) of
-        [] -> (lift $ (prettyPrint tabs ferror ("no provenance information for element "++(show term)++"\n"))) >> return []
-        skolemtrees -> do
-          -- Look at the first or all skolem trees depending on user input
-          case isall of
-            True -> do
-              let (actualelm,_,_) = head skolemtrees
-              let allelms = map (\(e, h, r)->e) skolemtrees
-              let skolemnext = concat (map (\(e, h, r)->r) skolemtrees)
-              let names = (map (\(e, h, r)->(actualelm, h, r)) skolemtrees) 
-              let namedthy = nameTheory thy names
-              lift $ prettyPrint tabs foutput ("origin of "++(show actualelm)++"(equal to "++(show allelms)++")... depends on origin of "++(show skolemnext)++"\n")
-              printDiff (thy,namedthy) ((show actualelm),tabs,isall)
-              return (map (\e->(Elem e)) skolemnext)
-            False -> do
-              let (elm, skolemhead, skolemnext) = (head skolemtrees)
-              let namedthy = (nameTheory thy [(elm, skolemhead, skolemnext)])
-              lift $ prettyPrint tabs foutput ("origin of "++(show elm)++"... depends on origin of "++(show skolemnext)++"\n")
-              printDiff (thy,namedthy) ((show elm),tabs,isall)
-              return (map (\e->(Elem e)) skolemnext)
-
--- Blaming
-justify :: Theory -> ProvInfo -> Model -> Formula -> InputT IO()
-justify theory prov model atom = case (getFact model atom) of
-  Nothing -> lift $ prettyPrint 0 ferror ("fact not in form FactName(e^0, e^1, ...) or is not in the current model\n") >> return ()
-  Just fact@(factname, factelms) -> case (getBlame prov model fact) of
-    [] -> lift $ prettyPrint 0 ferror ("no provenance information for fact "++(show atom)++"\n") >> return ()
-    blames -> do
-      let names = (concatMap (\t -> do
-                                      let skolemtrees = (getSkolemTrees prov model t) 
-                                      let (actualelm,_,_) = head skolemtrees
-                                      map (\(e, h, r)->(actualelm, h, r)) skolemtrees) factelms)
-      let blamedthy = (blameTheory theory names blames)
-      lift $ prettyPrint 0 foutput ("justification of "++(show atom)++"\n")
-      printDiff (theory,blamedthy) ((show atom),0,True)
-
--- Misc 
-printDiff :: (Theory, [Maybe Sequent]) -> (String, Int, Bool) -> InputT IO()
-printDiff (thy,dthy) format@(highlight,tabs,printall) = printDiffPlus (zip thy dthy) format
+printDiff :: (Theory, UTheorySubs) -> (String, Int, Bool) -> InputT IO()
+printDiff (theory,dtheory) format@(highlight,tabs,printall) = printDiffPlus (zip theory dtheory) format
 printDiffPlus :: [(Sequent, Maybe Sequent)] -> (String, Int, Bool) -> InputT IO()
 printDiffPlus [] _ = return ()
 printDiffPlus diff format@(highlight,tabs,printall) = do
@@ -142,8 +101,23 @@ printDiffPlus diff format@(highlight,tabs,printall) = do
   case ms of
     Nothing -> keepprinting
     Just ds -> do
-      lift $ prettyPrint tabs finput ("thy rule: "++(show s)++"\n")
+      lift $ prettyPrint tabs finput ("theory rule: "++(show s)++"\n")
       lift $ prettyHighlight tabs highlight ("instance: "++(show ds)++"\n")
       if printall
         then keepprinting
         else return ()
+
+
+justify :: UState -> Formula -> InputT IO()
+justify state@(theory, prov, stream, model) atom = case (getFact model atom) of
+  Nothing -> lift $ prettyPrint 0 ferror ("fact not in form FactName(e^0, e^1, ...) or is not in the current model\n") >> return ()
+  Just fact@(factname, factelms) -> case (getBlame prov model fact) of
+    [] -> lift $ prettyPrint 0 ferror ("no provenance information for fact "++(show atom)++"\n") >> return ()
+    blames -> do
+      let names = (concatMap (\t -> do
+                                      let skolemtrees = (getSkolemTrees prov model t) 
+                                      let (actualelm,_,_) = head skolemtrees
+                                      map (\(e, h, r)->(actualelm, h, r)) skolemtrees) factelms)
+      let blamedtheory = (blameTheory theory names blames)
+      lift $ prettyPrint 0 foutput ("justification of "++(show atom)++"\n")
+      printDiff (theory,blamedtheory) ((show atom),0,True)
