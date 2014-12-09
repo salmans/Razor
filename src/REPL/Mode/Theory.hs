632f7f6f847de4bacefb79952966db49367deed3
ryandanas <ryandanas@wpi.edu>
2014-12-09 00:01:06 -0500

    Fixes #47.

--- src/REPL/Mode/Theory.hs
+++ src/REPL/Mode/Theory.hs
@@ -32,6 +32,7 @@ import Text.ParserCombinators.Parsec
 import Text.Parsec.Prim
 import Syntax.GeometricParser
 import Syntax.GeometricUtils
+import Syntax.IGeometric
 
 instance LoopMode TheoryMode TheoryIn TheoryOut where
   runOnce	  = theoryRun
@@ -43,36 +44,47 @@ instance Mode TheoryMode where
   modeTag   = theoryTag
 
 data TheoryMode = TheoryM
-type TheoryIn = Config
-type TheoryOut = (Maybe Theory, Maybe ChaseState, ModelSpace, Maybe ModelCoordinate)
+type TheoryIn = (Config, Maybe Theory, Maybe ChaseState, ModelSpace, Maybe ModelCoordinate)
+type TheoryOut = (Config, Maybe Theory, Maybe ChaseState, ModelSpace, Maybe ModelCoordinate)
 
-data TheoryCommand = Load TheoryFile
+data TheoryCommand = Debug | Relax | DefaultDepth Int | Load TheoryFile 
 type TheoryFile = String
 
 --------------------
 -- Mode Functions --
 --------------------
 theoryRun :: TheoryMode -> TheoryIn -> String -> IO(Either Error TheoryOut)
-theoryRun mode config command = case parseTheoryCommand command of
+theoryRun mode (config, thy, gstar, mspace, mcoor) command = case parseTheoryCommand command of
   Left err -> return $ Left err
   Right cmd -> case cmd of
+    Debug -> do
+      let debug' = not (configDebug config)
+      prettyPrint 0 foutput $ "Debug mode is now "++(show debug')++"\n"
+      return $ Right $ (config {configDebug = debug'}, thy, gstar, mspace, mcoor)
+    Relax -> do
+      let relax' = not (configRelaxMin config)
+      prettyPrint 0 foutput $ "Relaxation of minimality constraint is now "++(show relax')++"\n"
+      return $ Right $ (config {configRelaxMin = relax'}, thy, gstar, mspace, mcoor)
+    DefaultDepth i -> do
+      prettyPrint 0 foutput $ "Default skolem depth is now "++(show i)++"\n"
+      return $ Right $ (config {configDefaultSkolemDepth = i}, thy, gstar, mspace, mcoor)
     Load file -> do
       load <- loadTheory config {configInput = Just file}
       case load of
         Right (thy', gs') -> do
           prettyTheory (Just thy')
           prettyPrint 0 foutput $ "Geometric theory loaded; ready to find models\n"
-          return $ Right $ (Just thy', Just gs', Map.empty, Nothing)
+          return $ Right $ (config, Just thy', Just gs', Map.empty, Nothing)
         Left err -> return $ Left err
 
 ------------------------
 -- RazorState Related --
 ------------------------
 updateTheory :: TheoryMode -> TheoryOut -> RazorState -> (RazorState, TheoryIn)
-updateTheory mode (theory', gstar', mspace', mcoor') state@(RazorState config theory gstar mspace mcoor) = (RazorState config theory' gstar' mspace' mcoor', config)
+updateTheory mode (config', theory', gstar', mspace', mcoor') state@(RazorState config theory gstar mspace mcoor) = (RazorState config' theory' gstar' mspace' mcoor', (config', theory', gstar', mspace', mcoor'))
 
 enterTheory :: TheoryMode -> RazorState -> IO(Either Error (TheoryOut))
-enterTheory mode state@(RazorState config theory gstar mspace mcoor) = return $ Right $ (theory, gstar, mspace, mcoor)
+enterTheory mode state@(RazorState config theory gstar mspace mcoor) = return $ Right $ (config, theory, gstar, mspace, mcoor)
 
 -----------------------
 -- Command Functions --
@@ -82,7 +94,10 @@ theoryTag mode = "%theory% "
 
 theoryHelp :: TheoryMode -> IO()
 theoryHelp cmd = prettyPrint 0 foutput $ ""++ 
-  "<expr>:= | load <string>     Load the given filename as an input theory\n"
+  "<expr>:= | debug             Toggle debug mode on/off\n"++
+  "         | relax             Toggle relaxation of producing only minimal models\n"++
+  "         | depth <int>       Set the default skolem depth to the given integer value\n"++
+  "         | load <string>     Load the given filename as an input theory\n"
 
 parseTheoryCommand :: String -> Either Error TheoryCommand
 parseTheoryCommand cmd = 
@@ -92,11 +107,29 @@ parseTheoryCommand cmd =
 		Right val -> Right $ val
 
 pCommand :: Parser TheoryCommand
-pCommand = pLoad
+pCommand = pDebug +++ pRelax +++ pDefaultDepth +++ pLoad 
+
+pDebug :: Parser TheoryCommand
+pDebug = do
+  symbol "debug"
+  return Debug
+
+pRelax :: Parser TheoryCommand
+pRelax = do
+  symbol "relax"
+  return Relax
+
+pDefaultDepth :: Parser TheoryCommand
+pDefaultDepth = do
+  symbol "depth"
+  spaces
+  DefaultDepth <$> pInt
+
+pInt :: Parser Int
+pInt = read <$> many1 digit
 
 pLoad :: Parser TheoryCommand
 pLoad = do
-	symbol "load"
-	spaces
-	Load <$> many (noneOf "\n\t ")
-
+  symbol "load"
+  spaces
+  Load <$> many (noneOf "\n\t ")
